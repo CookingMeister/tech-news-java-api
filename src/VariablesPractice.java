349b289ec3fac96e2b40151d3b7985c7710826e3
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-05-01 10:04:12 -0300

    create user, comment, post and vote models

---
+++ src/VariablesPractice.java
@@ -0,0 +1,10 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 20.0;
+        System.out.println(a + b);
+        String name = "Shawn";
+        String myVar = a + name;
+        System.out.println(myVar);
+    }
+}
